ebadcb8a0f774e7c6ed250fe609a15113f026b95
kevin-empire <60889197+zengyunjie@users.noreply.github.com>
2026-05-28 21:03:29 +0800

    Sync: app/vite.config.ts

---
+++ app/vite.config.ts
@@ -0,0 +1,21 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'plugin-inspect-react-code'
+
+export default defineConfig({
+  base: './',
+  plugins: [inspectAttr(), react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
